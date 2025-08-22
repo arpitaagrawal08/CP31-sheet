3187e150c04431598d62956d21c3f262576cb746
arpitaagrawal08 <168635973+arpitaagrawal08@users.noreply.github.com>
2025-08-23 01:15:21 +0530

    day-1

--- halloumi-boxes.cpp
+++ halloumi-boxes.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++)cin>>arr[i];
+        vector<int>dup=arr;
+        sort(dup.begin(),dup.end());
+        if(dup==arr||k>1) cout<<"yes"<<endl;
+        else{
+            cout<<"no"<<endl;
+            
+        }
+
+    }
     return 0;
 }
