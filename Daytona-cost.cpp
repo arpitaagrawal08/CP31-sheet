f404280d6222213880e33f02c7321bfd9f2a63a7
arpitaagrawal08 <168635973+arpitaagrawal08@users.noreply.github.com>
2025-08-24 23:23:18 +0530

    day3

---
+++ Daytona-cost.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)cin>>a[i];
+        string ans="NO";
+        for(int i:a){
+            if(i==k){
+                ans="YES";
+                break;
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
